581f3622187cb1662823314b83f734c48101e5d8
Lincoln-Dupree <lincoln.vfx@gmail.com>
2025-09-14 12:08:09 -0500

    good save point with the basic setup done, now onto making it work for any book!

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from stats import book_num_words
-
-
+from stats import book_num_char
+from stats import sort_dict
 
 def main():
+    # print(book_num_words(get_book_text("./books/frankenstein.txt")))
+    # print(book_num_char(get_book_text("./books/frankenstein.txt")))
+    # print(sort_dict(book_num_char(get_book_text("./books/frankenstein.txt"))))
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(book_num_words(get_book_text("./books/frankenstein.txt")))
+    print("--------- Character Count -------")
+    for each in sort_dict(book_num_char(get_book_text("./books/frankenstein.txt"))): 
+        print(f'{each[0]}: {each[1]}')
 
 def get_book_text(book_path):
     with open(book_path) as b:
--- stats.py
+++ stats.py
@@ -1,4 +1,24 @@
+from collections import Counter
+
 def book_num_words(book_contents):
     words_arr = book_contents.split()
     num_words = len(words_arr)
-    return f'{num_words} words found in the document'
+    return f'Found {num_words} total words'
+
+# def book_num_char(book_contents):
+#     lowercase_list = book_contents.lower()
+#     char_list = list(lowercase_list)
+#     default_count = 0
+#     unique_dict = dict.fromkeys(char_list, default_count)
+#     for char in char_list:
+#         if char in unique_dict:
+#             unique_dict[char] += 1
+
+#     return unique_dict
+
+def book_num_char(book_contents):
+    return Counter(book_contents.lower())
+
+def sort_dict(dict):
+   return sorted(dict.items(), key=lambda item: item[1], reverse=True)
+
