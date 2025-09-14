0175c9ce72cd543388590c76d7fff1b98a78a857
Lincoln-Dupree <lincoln.vfx@gmail.com>
2025-09-14 12:42:42 -0500

    project completed!

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import sys
 from stats import book_num_words
 from stats import book_num_char
 from stats import sort_dict
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def main():
     # print(book_num_words(get_book_text("./books/frankenstein.txt")))
     # print(book_num_char(get_book_text("./books/frankenstein.txt")))
     # print(sort_dict(book_num_char(get_book_text("./books/frankenstein.txt"))))
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
-    print(book_num_words(get_book_text("./books/frankenstein.txt")))
+    print(book_num_words(get_book_text(sys.argv[1])))
     print("--------- Character Count -------")
-    for each in sort_dict(book_num_char(get_book_text("./books/frankenstein.txt"))): 
+    for each in sort_dict(book_num_char(get_book_text(sys.argv[1]))): 
         print(f'{each[0]}: {each[1]}')
 
 def get_book_text(book_path):
